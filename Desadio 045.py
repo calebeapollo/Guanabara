48cf46edf5960102b7298972c20628b906c32be2
Calebe Apollo <apollocalebe@gmail.com>
2022-07-18 12:49:15 -0700

    exercicios

--- Desadio 045.py
+++ Desadio 045.py
@@ -1,32 +1,41 @@
-import time, random
+from random import randint
+import time
+itens=('Pedra','Papel','Tesoura')
+computador=randint(0,2)
 print('''SUAS OPÇÕES:
 [0] PEDRA
 [1] PAPEL
 [2] TESOURA''')
-n2=int(input('Qual sua jogada? ')) #jogada da pessoa
-
+jogador=int(input('Sua jogada: '))
 time.sleep(0.5)
 print('JO')
 time.sleep(0.5)
 print('KEN')
 time.sleep(0.5)
-print('PÔ!!!')
-PEDRA=0
-PAPEL=1
-TESOURA=2
-lista=[PEDRA,PAPEL,TESOURA]
-a=random.choice(lista) #escolha do compurador
-if a == 0:
-    print('Computador jogou PEDRA')
-elif a == 1:
-    print('Computador jogou PAPEL')
-elif a == 2:
-    print('Computador jogou TESOURA')
-if n2 == 0:
-    print('O jogador jogou PEDRA')
-elif n2 == 1:
-    print('O jogador jogou PAPEL')
-else:
-    print('O jogaor jogou TESOURA')
+print('PO')
 
-
+print('-'*30)
+print(f'Computador jogou {(itens[computador])}')
+print(f'Jogador jogou {(itens[jogador])}')
+print('-'*30)
+if computador == 0:
+  if jogador == 0:
+   print('Empate')
+  elif jogador == 1:
+   print('Jogador Ganhou!!')
+  else:
+      print('Computador Ganhou!!')
+elif computador == 1:
+     if jogador == 0:
+      print('Computador Ganhou!!')
+     elif jogador == 1:
+         print('Empate!!')
+     else:
+         print('Jogador Ganhou!!')
+elif computador == 2:
+    if jogador == 0:
+        print('Jogador Ganhou!!')
+    elif jogador == 1:
+        print('Computador Ganhou!!')
+    else:
+        print('Empate!!')
