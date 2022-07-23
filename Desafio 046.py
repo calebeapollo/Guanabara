81b6ab9a9d638582d5a51aa5ea20eedf89d858ab
Calebe Apollo <apollocalebe@gmail.com>
2022-07-22 17:16:35 -0700

    exercicios

--- Desafio 046.py
+++ Desafio 046.py
@@ -1,7 +1,2 @@
-import time
-time.sleep(1)
-print('CONTAGEM REGRESSIVA 🎆')
-for a in range (10,-1,-1):
-    time.sleep(1)
+for a in range (2,51,2):
     print(a)
-print('BOOOOM🥳🥳🥳')
---
+++ Desafio 047.py
@@ -0,0 +1,3 @@
+for a in range (2,51,2):
+    print(a,end=' ')
+print('Acabou!')
